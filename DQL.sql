8de36a339cb1cda832b9e3858af96322a33e6075
Enzo Okuizumi <enzookuizumifiap@gmail.com>
2025-11-21 19:12:17 -0300

    Tudo correto e atualizado

--- DQL.sql
+++ DQL.sql
@@ -1,31 +1,30 @@
 -- ===== DQL: consultas uteis =====
 
--- Lista pessoas que começam com a letra "a" em ordem alfabetica. A letra pode ser alterada.
+-- Lista pessoas que começam com a letra "a" em ordem alfabetica.
 SELECT id_user, nome, email 
 FROM ddd_user
-WHERE LOWER(nome) LIKE 'a%' -- Altere a letra
+WHERE LOWER(nome) LIKE 'a%' 
 ORDER BY nome;
 
 -- Lista o nome do modulo e a qual trilha pertence.
-SELECT m.id_modulo, m.nome AS modulo, t.nome AS trilha
+SELECT m.id_mod, m.nome AS modulo, t.nome AS trilha
 FROM ddd_modulo m
-JOIN ddd_trilha t ON m.id_trilha = t.id_trilha
+JOIN ddd_trilha t ON m.id_tri = t.id_tri
 ORDER BY t.nome;
 
 -- Lista o total de modulos por trilha
-SELECT t.nome AS trilha, COUNT(m.id_modulo) AS total_modulos
+SELECT t.nome AS trilha, COUNT(m.id_mod) AS total_modulos
 FROM ddd_trilha t
-LEFT JOIN ddd_modulo m ON t.id_trilha = m.id_trilha
+LEFT JOIN ddd_modulo m ON t.id_tri = m.id_tri
 GROUP BY t.nome
 ORDER BY total_modulos DESC;
 
-
 -- Lista todas as trilhas que possuem mais de um modulo cadastrado.
-SELECT t.nome AS trilha, COUNT(m.id_modulo) AS qtd_modulos
+SELECT t.nome AS trilha, COUNT(m.id_mod) AS qtd_modulos
 FROM ddd_trilha t
-JOIN ddd_modulo m ON t.id_trilha = m.id_trilha
+JOIN ddd_modulo m ON t.id_tri = m.id_tri
 GROUP BY t.nome
-HAVING COUNT(m.id_modulo) > 1
+HAVING COUNT(m.id_mod) > 1
 ORDER BY qtd_modulos DESC;
 
 -- Lista o progresso dos usuários por trilhas.
@@ -34,24 +33,24 @@ SELECT
     t.nome AS trilha,
     m.nome AS modulo,
     p.status AS progresso
-FROM ddd_progresso p
+FROM ddd_prog p
 JOIN ddd_user u 
     ON p.id_user = u.id_user
 JOIN ddd_modulo m 
-    ON p.id_modulo = m.id_modulo
+    ON p.id_mod = m.id_mod
 JOIN ddd_trilha t
-    ON m.id_trilha = t.id_trilha
-WHERE t.nome = 'Java Básico'   --  Altere aqui o nome da trilha
+    ON m.id_tri = t.id_tri
+WHERE t.nome = 'Frontend' --  Altere aqui o nome da trilha
 ORDER BY u.nome, m.nome;
 
--- Lista todas as sugestões ordenadas por dificuldade (facil → dificil)
+-- Lista todas as sugestões ordenadas por dificuldade
 SELECT titulo, tipo, dificuldade, duracao, link
-FROM ddd_sugestoes
+FROM ddd_sugs
 ORDER BY dificuldade;
 
 -- Conta quantas sugestões existem por nível de dificuldade
 SELECT dificuldade, COUNT(*) AS total
-FROM ddd_sugestoes
+FROM ddd_sugs
 GROUP BY dificuldade
 ORDER BY total DESC;
 
